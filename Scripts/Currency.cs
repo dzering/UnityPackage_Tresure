ecb82ee2e2017ce29bf74edd691513a20ce445c1
dzering <71943051+dzering@users.noreply.github.com>
2021-03-01 12:22:04 +0400

    Add files via upload

---
+++ Scripts/Currency.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Currency : MonoBehaviour
+    {
+        [SerializeField] private GameObject buttonMore;
+        [SerializeField] private GameObject buttonLess;
+        [SerializeField] private int Rate;
+        public int Amount = 0;
+        public Text textAmount;
+
+        public void ShowAmount()
+        {
+            textAmount.text = Amount.ToString();
+        }
+        private void Start()
+        {
+            ShowAmount();
+            textAmount = GetComponentInChildren<Text>();
+            Button btnLess = buttonLess.GetComponent<Button>();
+            btnLess.onClick.AddListener(() => Less());
+
+
+            Button btnMore = buttonMore.GetComponent<Button>();
+            btnMore.onClick.AddListener(() => More());
+        }
+
+        private void More()
+        {
+            Amount = Amount + Rate;
+            ShowAmount();
+
+        }
+
+        private void Less()
+        {
+            if(Amount == 0)
+            {
+                return;
+            }
+            Amount = Amount -    Rate;
+            ShowAmount();
+        }
+    }
+
+    
+}
+
